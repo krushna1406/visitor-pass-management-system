3a9d66298bcebe0fe5e6b07af8bc432363ea1596
krushna1406 <rautkrishna931@gmail.com>
2026-07-28 22:21:30 +0530

    Another debug commit | Configuring timedout options

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,8 +6,13 @@ const transporter = nodemailer.createTransport({
    secure: false,
    requireTLS: true,
 
+   connectionTimeout: 10000,
+   greetingTimeout: 10000,
+   socketTimeout: 10000,
+
    logger: true,
    debug: true,
+
    auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
@@ -18,21 +23,25 @@ const sendEmail = async ({ to, subject, html, attachments = [] }) => {
 
    console.log("EMAIL_USER:", process.env.EMAIL_USER);
    console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-   
+
    console.log('Verifying transporter...');
    transporter.verify();
 
    console.log('Transporter Verified');
 
-   const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
-      html,
-      attachments
-   })
+   try {
+      const info = await transporter.sendMail({
+         from: process.env.EMAIL_USER,
+         to,
+         subject,
+         html,
+         attachments
+      })
 
-   console.log(info)
+      console.log(info)
+   }catch(error){
+      console.error("sendMail error:", err);
+   }
    return info;
 }
 
